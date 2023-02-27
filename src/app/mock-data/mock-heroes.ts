b7a9731b28b3a40299cbf13f52eddc2b8474f57f
christianYdoow <christian.ramirez@novare.com.hk>
2023-02-27 13:52:14 +0800

    last commit

---
+++ src/app/mock-data/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from "../interfaces/hero";
+
+export const HEROES: Hero[]=[
+    {id:1,name:'superman',skill:'flying,super strength  '},
+    {id:2,name:'batman',skill:'rich'},
+    {id:3,name:'aquaman',skill:'communicate to see creatures'},
+    {id:4,name:'cyborg',skill:'technology'},
+    {id:5,name:'flash',skill:'super speed'},
+]
